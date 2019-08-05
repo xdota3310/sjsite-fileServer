982c0f01bce7ef688a1b3c42661b7308e80dbb28
xsj <shijie.xu@ucarinc.com>
2019-08-05 09:49:18 +0800

    init

---
+++ src/main/java/com/lr/fileserver/aop/LogAspect.java
@@ -0,0 +1,49 @@
+package com.lr.fileserver.aop;
+
+import com.lr.common.base.ResultResponse;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 负责日志记录
+ *
+ * @author shijie.xu
+ * @since 2019年08月02日
+ */
+@Aspect
+@Component
+public class LogAspect {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogAspect.class);
+    ServletRequestAttributes sa;
+    HttpServletRequest request;
+    /**
+     * 任意参数、任何名字的方法都打日志
+     */
+    @Pointcut("execution(public * com.lr.fileserver.web.controller..*.*(..))")
+    public void log(){}
+
+    @Before("log()")
+    public void doBefore(JoinPoint joinPoint){
+        sa = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        request = sa == null ? null : sa.getRequest();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-DD HH-mm-ss");
+        String now = simpleDateFormat.format(System.currentTimeMillis());
+        LOGGER.warn("Request:"+"[URI:"+request.getRequestURI()+"Time:"+now+"]");
+    }
+
+
+    @AfterReturning(returning = "res",pointcut = "log()")
+    public void doAfter(Object res){
+        LOGGER.warn("After:"+request.getRemoteAddr());
+    }
+}
---
+++ src/main/java/com/lr/fileserver/web/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.lr.fileserver.web.controller;
+
+import com.lr.common.base.ResultResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 请填写类注释
+ *
+ * @author shijie.xu
+ * @since 2019年08月02日
+ */
+@RestController
+public class TestController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+    @RequestMapping(value="/test",method=RequestMethod.GET)
+    @ResponseBody
+    public ResultResponse Test(){
+        return ResultResponse.createBySuccess();
+    }
+}
